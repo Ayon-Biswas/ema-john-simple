88de961b87f351219bc322d859e4c62a3310873d
Ayon-Biswas <ayonshirsho@gmail.com>
2021-03-07 01:48:56 +0600

    icon and styles of button

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee,faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import "./Product.css";
 const Product = (props) => {
     // console.log(props.product);
     // const {img, name} = props.product;
@@ -7,12 +9,18 @@ const Product = (props) => {
     return (
         <div className="product">
             <div>
-                <img src={props.product?.img} alt=""/>
+                <img src={props.product?.img} alt="" />
             </div>
             <div>
-            <h4>{props.product?.name}</h4>
+                <h4 className="product-name">{props.product?.name}</h4>
+                <br></br>
+                <p><small> by {props.product?.seller}</small></p>
+                <p>price:${props.product?.price}</p>
+                <br></br>
+                <p><small>only: {props.product?.stock} left in stock-order soon</small></p>
+                <button className="main-button"><FontAwesomeIcon icon={faShoppingCart} />add to cart</button>
             </div>
-            
+
         </div>
     );
 };
